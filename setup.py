a71c330ba65c1e7c346269708dd75ba8a005e671
gab <gabrielbordeaux@gmail.com>
2017-08-05 07:57:50 -0400

    Support for rollbacks

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except(IOError, ImportError):
 
 setup(
     name='dbschema',
-    version='1.0.2',
+    version='1.1',
     description='Schema migration made easy',
     long_description=long_description,
     author='Gabriel Bordeaux',
