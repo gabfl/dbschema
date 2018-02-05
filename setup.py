31802d3ef0fba4effdd47c154dde3d645e94925d
Gabriel Bordeaux <gabrielbordeaux@gmail.com>
2018-02-05 12:01:57 -0500

    Split queries into list (#7)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except(IOError, ImportError):
 
 setup(
     name='dbschema',
-    version='1.1.2',
+    version='1.2',
     description='Schema migration made easy',
     long_description=long_description,
     author='Gabriel Bordeaux',
