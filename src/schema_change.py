10d601dafa1f48b38fb24c19a9c68e3c145dc63a
gab <gabrielbordeaux@gmail.com>
2018-05-23 16:12:07 -0400

    test travix for pg10

--- src/schema_change.py
+++ src/schema_change.py
@@ -107,17 +107,16 @@ def get_mysql_connection(host, user, port, password, database, ssl):
         MySQL connection
     """
 
-    connection = pymysql.connect(host=host,
-                                 user=user,
-                                 port=port,
-                                 password=password,
-                                 db=database,
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor,
-                                 client_flag=pymysql.constants.CLIENT.MULTI_STATEMENTS,
-                                 ssl=ssl
-                                 )
-    return connection
+    return pymysql.connect(host=host,
+                           user=user,
+                           port=port,
+                           password=password,
+                           db=database,
+                           charset='utf8mb4',
+                           cursorclass=pymysql.cursors.DictCursor,
+                           client_flag=pymysql.constants.CLIENT.MULTI_STATEMENTS,
+                           ssl=ssl
+                           )
 
 
 def get_pg_connection(host, user, port, password, database, ssl):
@@ -125,17 +124,16 @@ def get_pg_connection(host, user, port, password, database, ssl):
         PostgreSQL connection
     """
 
-    connection = psycopg2.connect(host=host,
-                                  user=user,
-                                  port=port,
-                                  password=password,
-                                  dbname=database,
-                                  sslmode=ssl.get('sslmode', None),
-                                  sslcert=ssl.get('sslcert', None),
-                                  sslkey=ssl.get('sslkey', None),
-                                  sslrootcert=ssl.get('sslrootcert', None),
-                                  )
-    return connection
+    return psycopg2.connect(host=host,
+                            user=user,
+                            port=port,
+                            password=password,
+                            dbname=database,
+                            sslmode=ssl.get('sslmode', None),
+                            sslcert=ssl.get('sslcert', None),
+                            sslkey=ssl.get('sslkey', None),
+                            sslrootcert=ssl.get('sslrootcert', None),
+                            )
 
 
 def run_migration(connection, queries):
