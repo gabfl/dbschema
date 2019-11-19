67a7f6236012bc534b814b3c941a334e7acb6e80
Gabriel Bordeaux <gabrielbordeaux@gmail.com>
2019-11-18 19:15:37 -0500

    Make password optional (#23)
    * Make password optional
    
    * Bump version, disable invalid test

---
+++ src/unittest/utils/migrations_functional_test/postgresql/2018-12-12/up.sql
@@ -0,0 +1,6 @@
+-- Disabled, returns:
+-- psycopg2.errors.ActiveSqlTransaction: CREATE INDEX CONCURRENTLY cannot run inside a transaction block
+
+-- CREATE UNIQUE INDEX CONCURRENTLY dist_id_temp_idx ON distributors (dist_id);
+
+SELECT 1;
