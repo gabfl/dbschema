48f40929044625c9ad096cde133af4484c9a5b94
Gabriel Bordeaux <gabrielbordeaux@gmail.com>
2022-01-24 19:09:30 -0500

    Use pypandoc.convert_file

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup
 
-import pypandoc
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
 
 setup(
     name='dbschema',
     version='1.4.3',
     description='Schema migration made easy',
-    long_description=pypandoc.convert_file('README.md', 'rst'),
+    long_description=long_description,
     author='Gabriel Bordeaux',
     author_email='pypi@gab.lc',
     url='https://github.com/gabfl/dbschema',
